a8a4d92e40b877967b353bab9b95a93e0e4c4242
brennoviana <brennovianacoelho@gmail.com>
2024-08-16 14:30:05 -0300

    Commit inicial

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,6 @@
 import typescript from '@typescript-eslint/parser';
 import airbnb from 'eslint-config-airbnb-typescript';
 import eslintPluginImport from 'eslint-plugin-import';
-import eslintPluginReact from 'eslint-plugin-react';
-import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default [
@@ -14,8 +12,6 @@ export default [
     },
     plugins: {
       import: eslintPluginImport,
-      react: eslintPluginReact,
-      'jsx-a11y': eslintPluginJsxA11y,
       prettier: eslintPluginPrettier,
     },
     rules: {
@@ -23,11 +19,8 @@ export default [
       'prettier/prettier': 'error',
       'import/prefer-default-export': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+      'react/jsx-filename-extension': 'off',
+      'react/react-in-jsx-scope': 'off',
     },
   },
 ];
